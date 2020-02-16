dfa6f510db02509bdd89653c5c51382709887ea3
RZalmon <ranzlmn@gmail.com>
2020-02-16 21:44:20 +0200

    fix fb bug

--- js/upload-service.js
+++ js/upload-service.js
@@ -19,7 +19,7 @@ function uploadImg(elForm, ev) {
 
 function doUploadImg(elForm, onSuccess) {
     var formData = new FormData(elForm);
-    fetch('//://ca-upload.com/here/upload.php', {
+    fetch('//:ca-upload.com/here/upload.php', {
         method: 'POST',
         body: formData
     })
